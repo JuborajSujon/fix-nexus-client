fe3177b9c89f43c1f10e0bc2703029dffd7a68d9
Juboraj Sujon <mdsujon72@gmail.com>
2024-05-11 21:13:52 +0600

    Added loader at private router

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,12 +1,28 @@
 import { Navigate, useLocation } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
 import PropsTypes from "prop-types";
+import { RotatingLines } from "react-loader-spinner";
 
 const PrivateRoute = ({ children }) => {
   const { user, loading } = useAuth();
   const location = useLocation();
 
-  if (loading) return <p>Loading.....</p>;
+  if (loading)
+    return (
+      <div className="flex justify-center items-center py-32">
+        <RotatingLines
+          visible={true}
+          height="96"
+          width="96"
+          color="grey"
+          strokeWidth="5"
+          animationDuration="0.75"
+          ariaLabel="rotating-lines-loading"
+          wrapperStyle={{}}
+          wrapperClass=""
+        />
+      </div>
+    );
   if (user) return children;
 
   return <Navigate to="/login" state={location.pathname} replace={true} />;
