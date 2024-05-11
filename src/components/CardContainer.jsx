9f4db746a23b549e4e7ad25a0d5c445567bb144d
Juboraj Sujon <mdsujon72@gmail.com>
2024-05-11 23:33:49 +0600

    added service card padding

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import SectionTitle from "./SectionTitle";
 import ServiceCard from "./ServiceCard";
 
@@ -8,8 +9,24 @@ const CardContainer = () => {
         title="Popular Services"
         desctiption="Browse our popular services below and schedule an appointment with us today to get your gadgets working like new again."
       />
-      <div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
         <ServiceCard />
+        <ServiceCard />
+        <ServiceCard />
+        <ServiceCard />
+        <ServiceCard />
+        <ServiceCard />
+      </div>
+
+      <div className="flex justify-center mt-14">
+        <Link
+          to="/services"
+          className="relative p-0.5 inline-flex items-center justify-center font-bold overflow-hidden group rounded-md">
+          <span className="w-full h-full bg-gradient-to-br from-[#ff8a05] via-[#864508] to-[#0026ff] group-hover:from-[#ff6600c4] group-hover:via-[#545fff] group-hover:to-[#ff8a05] absolute"></span>
+          <span className="relative px-6 py-3 transition-all ease-out bg-gray-900 rounded-md group-hover:bg-opacity-0 duration-400">
+            <span className="relative text-white">Show All Services</span>
+          </span>
+        </Link>
       </div>
     </div>
   );
--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,58 +1,62 @@
+import { Link } from "react-router-dom";
+
 const ServiceCard = () => {
   return (
     <div>
-      <div className="overflow-hidden bg-white rounded-lg shadow-md dark:bg-gray-800">
-        <img
-          className="object-cover w-full h-64"
-          src="https://i.ibb.co/kMQ6NSw/Slider1.jpg"
-          alt="Article"
-        />
+      <div className="overflow-hidden bg-white rounded-lg shadow-md dark:bg-gray-800 ">
+        <div className="group">
+          <img
+            className="object-cover w-full h-64 group-hover:scale-110 transition-transform duration-300"
+            src="https://i.ibb.co/kMQ6NSw/Slider1.jpg"
+            alt="Article"
+          />
 
-        <div className="p-6">
-          <div>
-            <h2 className="block mt-2 text-xl font-semibold text-gray-800 transition-colors duration-300 transform dark:text-white hover:text-gray-600 hover:underline">
-              I Built A Successful Blog In One Year
-            </h2>
-            <p className="mt-2 text-sm text-gray-600 dark:text-gray-400">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Molestie
-              parturient et sem ipsum volutpat vel. Natoque sem et aliquam
-              mauris egestas quam volutpat viverra. In pretium nec senectus
-              erat. Et malesuada lobortis.
-            </p>
-            <h4 className="mt-2 text-xl font-bold text-gray-600 dark:text-gray-400">
-              Price: $50
-            </h4>
+          <div className="p-6">
+            <div>
+              <h2 className="block mt-2 text-xl font-semibold text-gray-800 transition-colors duration-300 transform dark:text-white hover:text-gray-600 hover:underline">
+                I Built A Successful Blog In One Year
+              </h2>
+              <p className="mt-2 text-sm text-gray-600 dark:text-gray-400">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Molestie parturient et sem ipsum volutpat vel. Natoque sem et
+                aliquam mauris egestas quam volutpat viverra. In pretium nec
+                senectus erat. Et malesuada lobortis.
+              </p>
+              <h4 className="mt-2 text-xl font-bold text-gray-600 dark:text-gray-400">
+                Price: $50
+              </h4>
+            </div>
+          </div>
+        </div>
+
+        <div className="mt-4 px-6 pb-6 flex items-start justify-between">
+          <div className="flex items-center">
+            <div className="flex items-center">
+              <img
+                className="object-cover w-12 h-12 rounded-full"
+                src="https://i.ibb.co/sRtRZmV/Slider3.jpg"
+                alt="Avatar"
+              />
+            </div>
+            <div className="ml-3">
+              <p className="text-sm text-gray-600 dark:text-gray-400">
+                Post By
+              </p>
+              <p className="text-xl text-gray-600 dark:text-gray-400">
+                John Doe
+              </p>
+            </div>
           </div>
 
-          <div className="mt-4 flex items-start justify-between">
-            <div className="flex items-center">
-              <div className="flex items-center">
-                <img
-                  className="object-cover w-12 h-12 rounded-full"
-                  src="https://i.ibb.co/sRtRZmV/Slider3.jpg"
-                  alt="Avatar"
-                />
-              </div>
-              <div className="ml-3">
-                <p className="text-sm text-gray-600 dark:text-gray-400">
-                  Post By
-                </p>
-                <p className="text-xl text-gray-600 dark:text-gray-400">
-                  John Doe
-                </p>
-              </div>
-            </div>
-
-            <div className="flex">
-              <button
-                type="submit"
-                className="relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-white rounded hover:bg-white group border">
-                <span className="w-48 h-48 rounded rotate-[-40deg] bg-orange-600 absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
-                <span className="relative w-full text-left text-black transition-colors duration-300 ease-in-out group-hover:text-white">
-                  Add Service
-                </span>
-              </button>
-            </div>
+          <div className="flex">
+            <Link
+              to="/services"
+              className="relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-white rounded hover:bg-white group border">
+              <span className="w-48 h-48 rounded rotate-[-40deg] bg-orange-600 absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
+              <span className="relative w-full text-left text-black transition-colors duration-300 ease-in-out group-hover:text-white">
+                View Details
+              </span>
+            </Link>
           </div>
         </div>
       </div>
