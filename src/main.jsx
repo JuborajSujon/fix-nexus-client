5bc77c78c4a7e08a0a57eea92fd95b4f66328ff0
Juboraj Sujon <mdsujon72@gmail.com>
2024-05-11 09:48:23 +0600

    Added firebase and auth provider

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,17 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/Routes";
 import { HelmetProvider } from "react-helmet-async";
+import AuthProvider from "./providers/AuthProvider";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <RouterProvider router={router} />
-    </HelmetProvider>
+    <AuthProvider>
+      <HelmetProvider>
+        <RouterProvider router={router} />
+        <ToastContainer />
+      </HelmetProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
