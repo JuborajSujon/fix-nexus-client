61564f1214faa7c8303d751230f8128753782957
Juboraj Sujon <mdsujon72@gmail.com>
2024-05-12 18:36:49 +0600

    Added pagination button

--- src/pages/AllServices.jsx
+++ src/pages/AllServices.jsx
@@ -40,6 +40,58 @@ const AllServices = () => {
         <ServiceCard />
         <ServiceCard />
       </div>
+
+      <div className="flex sticky bottom-5 items-center justify-center mt-10">
+        <a
+          href="#"
+          className="px-4 py-2 mx-1 capitalize bg-orange-400 text-slate-900 font-semibold rounded-md cursor-not-allowed hover:bg-blue-500 dark:hover:bg-blue-500 hover:text-white dark:hover:text-gray-200">
+          <div className="flex items-center -mx-1">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-6 h-6 mx-1 rtl:-scale-x-100"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor">
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M7 16l-4-4m0 0l4-4m-4 4h18"
+              />
+            </svg>
+
+            <span className="mx-1">previous</span>
+          </div>
+        </a>
+
+        <a
+          href="#"
+          className="hidden px-4 py-2 mx-1 transition-colors duration-300 transform bg-orange-400 text-slate-900 font-semibold rounded-md sm:inline  hover:bg-blue-500 dark:hover:bg-blue-500 hover:text-white dark:hover:text-gray-200">
+          1
+        </a>
+
+        <a
+          href="#"
+          className="px-4 py-2 mx-1 text-slate-900 font-semibold transition-colors duration-300 transform bg-orange-400 rounded-md  hover:bg-blue-500 dark:hover:bg-blue-500 hover:text-white dark:hover:text-gray-200 pr-7">
+          <div className="flex items-center -mx-1">
+            <span className="mx-1">Next</span>
+
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-6 h-6 mx-1 rtl:-scale-x-100"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor">
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M17 8l4 4m0 0l-4 4m4-4H3"
+              />
+            </svg>
+          </div>
+        </a>
+      </div>
     </div>
   );
 };
