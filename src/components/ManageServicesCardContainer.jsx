3d6a11002b84634d9ad238dc6dbbb0fb97fe918d
Juboraj Sujon <mdsujon72@gmail.com>
2024-05-13 23:05:14 +0600

    Added sweet alert delete confirmation before delete item

--- src/components/ManageServicesCardContainer.jsx
+++ src/components/ManageServicesCardContainer.jsx
@@ -3,6 +3,7 @@ import ManageServiceCard from "./ManageServiceCard";
 import useAxiosGeneral from "../hooks/useAxiosGeneral";
 import useAuth from "../hooks/useAuth";
 import { toast } from "react-toastify";
+import Swal from "sweetalert2";
 
 const ManageServicesCardContainer = () => {
   const [services, setServices] = useState([]);
@@ -20,12 +21,26 @@ const ManageServicesCardContainer = () => {
   // Delete Service
   const handleDelete = async (id) => {
     try {
-      const { data } = await axiosGeneral.delete(`/services/${id}`);
-      if (data.acknowledged) {
-        toast.success("Service Deleted Successfully", { autoClose: 1500 });
-        const newServices = services.filter((service) => service._id !== id);
-        setServices(newServices);
-      }
+      Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!",
+      }).then(async (result) => {
+        if (result.isConfirmed) {
+          const { data } = await axiosGeneral.delete(`/services/${id}`);
+          if (data.acknowledged) {
+            toast.success("Service Deleted Successfully", { autoClose: 1500 });
+            const newServices = services.filter(
+              (service) => service._id !== id
+            );
+            setServices(newServices);
+          }
+        }
+      });
     } catch (error) {
       console.log(error.message);
       toast.error(error.message);
